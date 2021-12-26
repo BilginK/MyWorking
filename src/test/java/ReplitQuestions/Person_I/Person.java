a065e379160ba321de385ec493dc5e0798e46900
BilginK <bilginkalmis@gmail.com>
2021-12-26 20:35:11 +0100

    Person_I solution has a small changing.

--- src/test/java/ReplitQuestions/Person_I/Person.java
+++ src/test/java/ReplitQuestions/Person_I/Person.java
@@ -9,7 +9,6 @@ public class Person {
         this.firstName = "undefined";
         this.lastName = "undefined";
         this.age= -1;
-
     }
     public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
@@ -24,16 +23,13 @@ public class Person {
     }
     public String getLastName() {
         return lastName;
-
     }
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
